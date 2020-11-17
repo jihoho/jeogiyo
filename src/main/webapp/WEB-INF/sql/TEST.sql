6b97becf969ae06c47fada709927134d1371313f
jihoho <xkftn94@naver.com>
2020-11-17 16:38:01 +0900

    Add .sql

---
+++ src/main/webapp/WEB-INF/sql/TEST.sql
@@ -0,0 +1,31 @@
+insert into SHOP_OWNER( SHOP_OWNER_ID, SHOP_OWNER_PW, SHOP_OWNER_NAME, SHOP_OWNER_GENDER, 
+                        TEL1,TEL2,TEL3,HP1,HP2,HP3,SMSSTS_YN,EMAIL1,EMAIL2,EMAILSTS_YN,
+                        ZIPCODE,ROAD_ADDRESS,JIBEON_ADDRESS,DETAIL_ADDRESS,
+                        BIRTH_Y,BIRTH_M,BIRTH_D,BIRTH_GN,DEL_YN)
+        VALUES('jihoho','ghwns12','지호준','남',
+                '031','111','1234','010','111','1111',0,'jihoho4589','gmail.com',0,
+                '1234','경기도 용인시 수지구 풍덕천로 111','경기도 용인시 수지구 풍덕천동 1075-3','7층',
+                '1994','09','13',0,0);    
+                    
+select * from shop_owner;               
+                
+                
+
+insert into SHOP(SHOP_ID, SHOP_NAME,ROAD_ADDRESS,JIBEON_ADDRESS,DETAIL_ADDRESS,
+                ZIPCODE,TEL1,TEL2,TEL3,HP1,HP2,HP3,MIN_ORDER_PRICE,DELIVERY_TIP,
+                DELIVERY_MIN_TIME,DELIVERY_MAX_TIME,SHOP_OWNER_ID,SHOP_REGIS_NUMBER,
+                SHOP_INTRO,OPEN_TIME_H,OPEN_TIME_M,CLOSE_TIME_H,CLOSE_TIME_M)
+        VALUES(SEQ_SHOP_ID.NEXTVAL,'네네치킨-수지2지구점','경기도 용인시 수지구 풍덕천동 1047-3','경기도 용인시 수지구 정평로 40-3','거묵상가2 111호',
+                '4321','031','111','1234','010','111','1111',15000,2000,
+                40,50,'jihoho','1420915566',
+                '리뷰 이벤트 !!
+                ####미니 핫도그도 공짜
+                리뷰란 이벤트 내용참고하세요!
+                네네치킨은 건강한 환경에서 건강하게 자란 100% 국내산 닭고기를 사용합니다 ~!',11,30,00,30);   
+                
+                
+select shop_intro from shop;
+
+insert into SHOP_CATEGORY(SHOP_CATEGORY_NAME,SHOP_ID)
+        values(
+                "chicken'
---
+++ src/main/webapp/WEB-INF/sql/jeogiyo_table_schema.sql
@@ -0,0 +1,172 @@
+DROP TABLE FOOD_IMAGE
+DROP TABLE SHOP_CATEGORY;
+DROP TABLE DELIVERY_AREA;
+DROP TABLE FOOD;
+DROP TABLE DIB;
+DROP TABLE REVIEW;
+DROP TABLE SHOP CASCADE CONSTRAINTS;
+DROP TABLE SHOP_OWNER CASCADE CONSTRAINTS;
+SELECT * FROM SHOP 
+--------------------------------------------------------
+--  DDL for Table SHOP_OWNER
+--------------------------------------------------------
+
+  CREATE TABLE "SHOP_OWNER" 
+   (	"SHOP_OWNER_ID" VARCHAR2(20 BYTE) primary key, 
+	"SHOP_OWNER_PW" VARCHAR2(30 BYTE), 
+	"SHOP_OWNER_NAME" VARCHAR2(50 BYTE), 
+	"SHOP_OWNER_GENDER" VARCHAR2(10 BYTE), 
+	"TEL1" VARCHAR2(20 BYTE), 
+	"TEL2" VARCHAR2(20 BYTE), 
+	"TEL3" VARCHAR2(20 BYTE), 
+	"HP1" VARCHAR2(20 BYTE), 
+	"HP2" VARCHAR2(20 BYTE), 
+	"HP3" VARCHAR2(20 BYTE), 
+	"SMSSTS_YN"  NUMBER(1) CHECK(SMSSTS_YN IN(0,1)), 
+	"EMAIL1" VARCHAR2(20 BYTE), 
+	"EMAIL2" VARCHAR2(20 BYTE), 
+	"EMAILSTS_YN" NUMBER(1) CHECK(EMAILSTS_YN IN(0,1)), 
+	"ZIPCODE" VARCHAR2(20 BYTE), 
+	"ROAD_ADDRESS" VARCHAR2(500 BYTE), 
+	"JIBEON_ADDRESS" VARCHAR2(500 BYTE), 
+	"DETAIL_ADDRESS" VARCHAR2(500 BYTE), 
+	"BIRTH_Y" VARCHAR2(20 BYTE), 
+	"BIRTH_M" VARCHAR2(20 BYTE), 
+	"BIRTH_D" VARCHAR2(20 BYTE), 
+	"BIRTH_GN" NUMBER(1) CHECK(BIRTH_GN IN(0,1)), 
+	"JOIN_DATE" DATE DEFAULT sysdate, 
+	"DEL_YN" NUMBER(1) DEFAULT 0
+   ) ;
+   
+--------------------------------------------------------
+--  DDL for Table SHOP
+--------------------------------------------------------
+
+    CREATE SEQUENCE  "SEQ_SHOP_ID"  MINVALUE 1 MAXVALUE 10000000 INCREMENT BY 1 START WITH 400 NOCACHE ORDER  NOCYCLE ;
+    
+    CREATE TABLE "SHOP" 
+    (
+        "SHOP_ID" NUMBER(20,0) primary key,
+        "SHOP_NAME" VARCHAR2(100 BYTE),
+        "ROAD_ADDRESS" VARCHAR2(500 BYTE),
+        "JIBEON_ADDRESS" VARCHAR2(500 BYTE),
+        "DETAIL_ADDRESS" VARCHAR2(500 BYTE),
+        "ZIPCODE" VARCHAR2(20 BYTE),
+        "TEL1" VARCHAR2(20 BYTE), 
+        "TEL2" VARCHAR2(20 BYTE), 
+        "TEL3" VARCHAR2(20 BYTE), 
+        "HP1" VARCHAR2(20 BYTE), 
+        "HP2" VARCHAR2(20 BYTE), 
+        "HP3" VARCHAR2(20 BYTE),
+        "MIN_ORDER_PRICE" NUMBER(10,0),
+        "DELIVERY_TIP" NUMBER(10,0),
+        "DELIVERY_MIN_TIME" NUMBER(10,0),
+        "DELIVERY_MAX_TIME" NUMBER(10,0),
+        "SHOP_OWNER_ID" VARCHAR2(20 BYTE) REFERENCES SHOP_OWNER(SHOP_OWNER_ID),
+        "SHOP_REGIS_NUMBER" VARCHAR2 (30 BYTE),
+        "STAR_AVG" NUMBER(10,1) DEFAULT 0,
+        "STAR_CNT" NUMBER(10,0) DEFAULT 0,
+        "DIB_CNT" NUMBER(10,0) DEFAULT 0,
+        "REVIEW_CNT" NUMBER(10,0) DEFAULT 0,
+        "SHOP_INTRO" CLOB,
+        "OPEN_TIME_H" NUMBER(3,0),
+        "OPEN_TIME_M" NUMBER(3,0),
+        "CLOSE_TIME_H" NUMBER(3,0),
+        "CLOSE_TIME_M" NUMBER(3,0)
+        
+    );
+    
+    
+
+--------------------------------------------------------
+--  DDL for Table SHOP_CATEGORY
+--------------------------------------------------------  
+
+    CREATE TABLE "SHOP_CATEGORY" 
+    (
+        "SHOP_CATEGORY_NAME" VARCHAR2(100 BYTE),
+        "SHOP_ID" NUMBER(20,0) REFERENCES SHOP(SHOP_ID)
+        
+    );
+    
+--------------------------------------------------------
+--  DDL for Table SHOP_IMAGE
+--------------------------------------------------------  
+
+    CREATE TABLE "SHOP_IMAGE"
+    (
+        "SHOP_IMAGE_ID" NUMBER(20,0),
+        "SHOP_ID" NUMBER(20,0) REFERENCES SHOP(SHOP_ID),
+        "IMAGE_FILE_NAME" VARCHAR2(200),
+        "REG_DATE" DATE
+    );
+
+--------------------------------------------------------
+--  DDL for Table DELIVERY_AREA
+--------------------------------------------------------  
+
+    CREATE TABLE "DELIVERY_AREA" 
+    (
+        
+        "DO_ADDRESS" VARCHAR2(100 BYTE),
+        "SI_ADDRESS" VARCHAR2(100 BYTE),
+        "GU_ADDRESS" VARCHAR2(100 BYTE),
+        "DONG_ADDRESS" VARCHAR2(100 BYTE),
+        "SHOP_ID" NUMBER(20,0) REFERENCES SHOP(SHOP_ID)
+        
+    );
+    
+--------------------------------------------------------
+--  DDL for Table DIB
+--------------------------------------------------------  
+
+--    CREATE TABLE "SHOP_DIB" 
+--    (
+--        "DONG_ADDRESS" VARCHAR2(100 BYTE),
+--        "SHOP_ID" NUMBER(20,0) REFERENCES SHOP(SHOP_ID)
+--        
+--    );
+    
+    
+    
+    
+    
+    
+--------------------------------------------------------
+--  DDL for Table FOOD
+--------------------------------------------------------  
+
+    CREATE TABLE "FOOD" 
+    (
+        "FOOD_ID" NUMBER(20,0) PRIMARY KEY,
+        "FOOD_NAME" VARCHAR2(200 BYTE),
+        "FOOD_DESC" CLOB,
+        "FOOD_PRICE" NUMBER(20,0),
+        "SHOP_ID" NUMBER(20,0) REFERENCES SHOP(SHOP_ID)
+        
+    );
+    
+--------------------------------------------------------
+--  DDL for Table FOOD_CATEGORY
+--------------------------------------------------------  
+    CREATE TABLE "FOOD_CATEGORY" 
+    (
+        "FOOD_CATEG_NAME" VARCHAR2(100 BYTE),
+        "FOOD_ID" NUMBER(20,0) REFERENCES FOOD(FOOD_ID)
+        
+    );
+
+--------------------------------------------------------
+--  DDL for Table FOOD_IMAGE
+--------------------------------------------------------  
+    CREATE TABLE "FOOD_IMAGE" 
+    (
+        "FOOD_IMAGE_ID" NUMBER(20,0) PRIMARY KEY,
+        "FOOD_ID" NUMBER(20,0) REFERENCES FOOD(FOOD_ID),
+        "IMAGE_FILE_NAME" VARCHAR2(200),
+        "REG_DATE" DATE DEFAULT sysdate
+        
+    );
+
+
+
