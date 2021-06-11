cd2de0bcd207d9649dc3b67dca62d9d39344333e
jihoho <xkftn94@naver.com>
2021-06-11 18:56:24 +0900

    Refac : Member DAO,Service 접근 제어 default로 수정

--- src/main/java/com/jeogiyo/member/service/MemberService.java
+++ src/main/java/com/jeogiyo/member/service/MemberService.java
@@ -1,19 +1,20 @@
 package com.jeogiyo.member.service;
 
+import com.jeogiyo.member.dto.MemberInfoDto;
 import com.jeogiyo.member.vo.MemberVO;
 
 import java.util.Map;
 
 public interface MemberService {
-    public String overlapped(Map<String,String> idMap) throws Exception;
+    String overlapped(Map<String,String> idMap) throws Exception;
 
-    public void addMember(MemberVO memberVO) throws Exception;
+    void addMember(MemberVO memberVO) throws Exception;
 
-    public MemberVO login(Map<String, String> loginMap) throws  Exception;
+    MemberInfoDto login(Map<String, String> loginMap) throws  Exception;
 
-    public void modifyMember(MemberVO memberVO) throws Exception;
+    void modifyMember(MemberVO memberVO) throws Exception;
 
-    public String getMemberSaltByIdAndType(String member_id,String member_type) throws Exception;
+    String getMemberSaltByIdAndType(String memberId,String memberType) throws Exception;
 
-    public void modifyMemberExcludePw(MemberVO memberVO) throws Exception;
+    void modifyMemberExcludePw(MemberVO memberVO) throws Exception;
 }
